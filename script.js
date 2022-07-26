ad8429bd92683611181e647f0ed30ba7178d8836
peter <peter_cook@comcast.com>
2022-07-26 17:50:04 -0400

    chapter27Studio

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-// TODO: add code here
+// TODO: 
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+      response.json().then(function(data){
+          console.log(data)
+        let divNode = document.getElementById("container");
+        for(let i =0; i<data.length; i++){
+        divNode.innerHTML += 
+        `<div class="astronaut">
+        <div class="bio">
+        <h3> ${data[i].firstName}</h3>
+        <h3> ${data[i].lastName}</h3>
+        <ul>
+        <li> ${data[i].hoursInSpace}</li>
+        <li> ${data[i].active}</li>
+        <li> ${data[i].skills}</li>
+    </ul>
+    </div>
+    <img class="avatar" src="${data[i].picture}">
+    </div>`
+        
+        
+    };
+    });
+  });
+});
+
+
